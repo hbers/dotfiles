630192001a0fd47699a653de16c637d0590f0f8d
hbers <hbersp@gmail.com>
2015-08-14 00:06:12 +0300

    proper translation

--- mpv/scripts/altyazi.lua
+++ mpv/scripts/altyazi.lua
@@ -4,18 +4,18 @@
 local utils = require 'mp.utils'
 function load_sub_fn()
     subl = "/usr/bin/subberthehut" -- use 'which subliminal' to find the path
-    mp.msg.info("Searching subtitle")
+    mp.msg.info("Altyazı aranıyor")
     mp.osd_message("Altyazı aranıyor")
     t = {}
     t.args = {subl,"--same-name", "--", mp.get_property("path")}
     res = utils.subprocess(t)
     if res.status == 0 then
         mp.commandv("rescan_external_files", "reselect") 
-        mp.msg.info("Subtitle download succeeded")
+        mp.msg.info("Altyazı indirildi")
         mp.osd_message("Altyazı indirildi")
     else
-        mp.msg.warn("Subtitle download failed")
-        mp.osd_message("Altyazı indirilemedi")
+        mp.msg.warn("Altyazı indirilmedi")
+        mp.osd_message("Altyazı indirilmedi")
     end
 end
 
